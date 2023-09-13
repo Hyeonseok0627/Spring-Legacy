acdc886636ce0ebc43d024dd726bf04182ae6118
Hyeonseok0627 <stonehead0627@gmail.com>
2023-09-13 15:49:06 +0900

    수업내용 pro26 완료(230913)

---
+++ pro26/src/com/spring/ex02/LoginController.java
@@ -0,0 +1,122 @@
+package com.spring.ex02;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller("loginController")
+public class LoginController {
+	@RequestMapping(value = { "/test/loginForm.do", "/test/loginForm2.do" }, method = { RequestMethod.GET })
+	public ModelAndView loginForm(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("loginForm");
+		return mav;
+	}
+	
+    @RequestMapping(value = "/test/login.do", method={RequestMethod.GET,RequestMethod.POST})
+	public ModelAndView login(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		request.setCharacterEncoding("utf-8");
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("result");
+		String userID = request.getParameter("userID");
+		String userName = request.getParameter("userName");
+		mav.addObject("userID", userID);
+		mav.addObject("userName", userName);
+
+		return mav;
+	}
+
+    //데이터 가져오기 형식 1, 단수, 낱개로 가져오기.
+    //확인이 로그인 폼에서 하니, 일단, 뷰를 서버에 올리기.
+    //스프링 구조이니, 폼도, 무조건 서버에서 등록된 주소로 가야함
+	/*
+    @RequestMapping(value = "/test/login2.do", method = { RequestMethod.GET, RequestMethod.POST })
+    //()에 있는 값은 로그인 폼 값의 name과 같게, 뒤에 있는 값은 가져오는 name값을 재할당하는 것이라 이름을 마음대로 변경해서 지정해도됨
+	public ModelAndView login2(@RequestParam("userID") String userID, 
+			                  @RequestParam("userName") String userName,
+			                  HttpServletRequest request, HttpServletResponse response) throws Exception {
+		request.setCharacterEncoding("utf-8");
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("result");
+		
+		// String userID = request.getParameter("userID");
+		// String userName = request.getParameter("userName");
+		
+		System.out.println("userID: "+userID);
+		System.out.println("userName: "+userName);
+		mav.addObject("userID", userID);
+		mav.addObject("userName", userName);
+
+		return mav;
+	}*/
+	
+
+	@RequestMapping(value = "/test/login2.do", method = { RequestMethod.GET, RequestMethod.POST })
+	public ModelAndView login2(@RequestParam("userID") String userID, 
+                               @RequestParam(value="userName", required=true) String userName,
+			                   @RequestParam(value="email", required=false) String email,
+			                  HttpServletRequest request, HttpServletResponse response) throws Exception {
+		request.setCharacterEncoding("utf-8");
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("result");
+		
+		// String userID = request.getParameter("userID");
+		// String userName = request.getParameter("userName");
+		
+		//결과 확인은 콘솔로 확인하기.
+		System.out.println("userID: "+userID);
+		System.out.println("userName: "+userName);
+		System.out.println("email: "+ email);
+		mav.addObject("userID", userID);
+		mav.addObject("userName", userName);
+		return mav;
+	}
+	
+	
+	@RequestMapping(value = "/test/login3.do", method = { RequestMethod.GET, RequestMethod.POST })
+	public ModelAndView login3(@RequestParam Map<String, String> info,
+			                   HttpServletRequest request, HttpServletResponse response) throws Exception {
+		request.setCharacterEncoding("utf-8");
+		ModelAndView mav = new ModelAndView();
+		
+		String userID = info.get("userID");
+		String userName = info.get("userName");
+		String userEmail = info.get("email");
+		System.out.println("userID: "+userID);
+		System.out.println("userName: "+userName);
+		System.out.println("userEmail: "+userEmail);
+		
+		mav.addObject("info", info);
+		mav.setViewName("result");
+		return mav;
+	}
+	
+	/*@RequestMapping(value = "/test/login4.do", method = { RequestMethod.GET, RequestMethod.POST })
+	public ModelAndView login4(@ModelAttribute("info") LoginVO loginVO,
+			                   HttpServletRequest request, HttpServletResponse response) throws Exception {
+		request.setCharacterEncoding("utf-8");
+		ModelAndView mav = new ModelAndView();
+		System.out.println("userID: "+loginVO.getUserID());
+		System.out.println("userName: "+loginVO.getUserName());
+		mav.setViewName("result");
+		return mav;
+	}*/
+	   
+	@RequestMapping(value = "/test/login5.do", method = { RequestMethod.GET, RequestMethod.POST })
+	public String login5(Model model,
+			                   HttpServletRequest request, HttpServletResponse response) throws Exception {
+		request.setCharacterEncoding("utf-8");
+		model.addAttribute("userID", "hong");
+		model.addAttribute("userName", "ȫ�浿");
+		return "result";
+	}	
+}
